3f95b59713a7803178dadbf73bc988d46e764ffe
Wanny Martins <wcatery@gmail.com>
2022-02-18 16:21:57 -0300

    adicionando PlanetCard no componente SolarSystem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
+import PlanetCard from './components/PlanetCard';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
 
 class App extends React.Component {
   render() {
@@ -9,6 +9,7 @@ class App extends React.Component {
       <>
         <Header />
         <SolarSystem />
+        <PlanetCard />
       </>
     );
   }
