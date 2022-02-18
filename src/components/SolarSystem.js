df57b945dc9e04e37ce1de3450c8bf5e60d09732
Wanny Martins <wcatery@gmail.com>
2022-02-18 16:43:46 -0300

    adicionando o componente missions e renderizando dentro de Title

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -12,7 +12,7 @@ class SolarSystem extends React.Component {
           <PlanetCard
             planetName={ planet.name }
             planetImage={ planet.image }
-            key={ planet.index }
+            key={ planet.name }
           />))}
       </div>
     );
